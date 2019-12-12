3cc723cd5283b31e52a668516fd1b50a4ba05bee
seongjae.kwon <seongjae.kwon@nhnent.com>
2019-12-12 13:43:16 +0900

    재귀적 정의, 파이썬 제너레이터 활용 추가

--- Functional/paradigm_division.py
+++ Functional/paradigm_division.py
@@ -5,7 +5,7 @@
 # 1. 절차형
 # 1.1. 엄격한 절차형
 result = 0
-for n in range(1, 10):
+for n in range(1, 20):
     if n % 5 == 0\
     or n % 7 == 0:
         result += n
@@ -13,7 +13,7 @@ print(result)
 
 # 1.2. 파이썬의 OOP를 활용
 result = list()
-for n in range(1, 10):
+for n in range(1, 20):
     if n % 5 == 0\
     or n % 7 == 0:
         result.append(n)
@@ -30,8 +30,34 @@ class SummableList(list):
 
 
 result = SummableList()
-for n in range(1, 10):
+for n in range(1, 20):
     if n % 5 == 0\
     or n % 7 == 0:
         result.append(n)
 print(result.sum())
+
+
+# 2. 함수형 패러다임
+
+# 2.1. 재귀적 정의
+# 마치 점화식과 비슷하다!
+def sum_(seq):
+    if len(seq) == 0:
+        return 0 # 끝나는 조건
+    return seq[0] + sum_(seq[1:]) # 재귀 조건
+
+
+def until(n, filter_func, v):
+    if v == n:
+        return [] # 끝나는 조건
+    if filter_func(v):
+        return [v] + until(n, filter_func, v+1)
+    else:
+        return until(n, filter_func, v+1)
+
+
+print(until(20, lambda  x: x%5 == 0 or x%7 == 0, 0))
+
+
+# 2.2. 함수형 하이브리드
+print(sum(n for n in range(1, 20) if n%5 == 0 or n%7==0))
